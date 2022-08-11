9dcbc986a0eab12a07c8b855ff412f55daa7066f
tbrincatos <tamsinbrincatos@gmail.com>
2022-08-11 18:05:52 +0200

    Included google fonts + cleaned docs
    - included Google fonts
    - cleaned files and deleted index.js
    - added header stuff and added font-family to make sure the Google fonts work

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
+import React from "react";
 import "./App.css";
-
-function App() {
+export default function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h3>Maltisian</h3>
         <h2>Dictionary App</h2>
       </header>
     </div>
   );
 }
-
-export default App;
