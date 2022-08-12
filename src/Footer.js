738a56c31caba11e263c6550e0ccea9d0229c4f2
tbrincatos <tamsinbrincatos@gmail.com>
2022-08-12 16:53:43 +0200

    removed redundant footer element as it was present in the App.js

--- src/Footer.js
+++ src/Footer.js
@@ -3,29 +3,27 @@ import "./Footer.css";
 export default function Footer() {
   return (
     <div className="Footer">
-      <footer>
-        <a
-          href="https://github.com/tbrincatos/weather-app-gentilly"
-          target="_blank"
-          rel="noopener noreferrer"
-          title="Go to GitHub account"
-        >
-          Open-source code
-        </a>{" "}
-        by{" "}
-        <a
-          href="https://chimerical-babka-82bcda.netlify.app/"
-          target="_blank"
-          rel="noopener noreferrer"
-          title="Visit Portfolio"
-        >
-          Tamsin Brincat
-        </a>
-        <span role="img" aria-label="girl">
-          {" "}
-          👩‍🦰
-        </span>
-      </footer>
+      <a
+        href="https://github.com/tbrincatos/weather-app-gentilly"
+        target="_blank"
+        rel="noopener noreferrer"
+        title="Go to GitHub account"
+      >
+        Open-source code
+      </a>{" "}
+      by{" "}
+      <a
+        href="https://chimerical-babka-82bcda.netlify.app/"
+        target="_blank"
+        rel="noopener noreferrer"
+        title="Visit Portfolio"
+      >
+        Tamsin Brincat
+      </a>
+      <span role="img" aria-label="girl">
+        {" "}
+        👩‍🦰
+      </span>
     </div>
   );
 }
