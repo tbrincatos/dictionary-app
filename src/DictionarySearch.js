6041f8bca9d30e4cf536d86ac6b600e384d59b5a
tbrincatos <tamsinbrincatos@gmail.com>
2022-08-15 12:18:55 +0200

    integrated API
    - installed and imported axios
    - created a function with the apicall
    - called the function within the onSubmit function
    - deleted icon stuff in manifest.json

--- src/DictionarySearch.js
+++ src/DictionarySearch.js
@@ -1,10 +1,18 @@
 import React, { useState } from "react";
-
+import axios from "axios";
 export default function DictionarySearch() {
   const [input, setInput] = useState(null);
+  function handleData(response) {
+    console.log(response.data[0]);
+  }
+  function makeApiCall() {
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${input}`;
+    axios.get(apiUrl).then(handleData);
+  }
   function handleSubmit(event) {
     event.preventDefault();
     alert(`Searching for ${input}`);
+    makeApiCall();
   }
   function handleChange(event) {
     setInput(event.target.value);
