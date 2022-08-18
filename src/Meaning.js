86b0b65ddeb72f3a6be5359dccbe7be1e17cf20d
tbrincatos <88725546+tbrincatos@users.noreply.github.com>
2022-08-18 17:13:06 +0200

    worked on styling
    - added sections to ease the styling
     - App.css: removed the unneccessary color variables in the root and pudated the secondary
    - Meaning.js : removed bootstrap margin
    - Meaning.css : worked on the styling of definition and example and added media queries
    - Played with the secondary color and changed it in Phonetics.css
    - Results.css: played with the section styling, the margins of the page and media queries

--- src/Meaning.js
+++ src/Meaning.js
@@ -4,20 +4,22 @@ import "./Meaning.css";
 export default function Meaning(props) {
   return (
     <div className="Meaning">
-      <h4 className="part-of-speech">{props.meaning.partOfSpeech}</h4>
-      <div>
-        {props.meaning.definitions.map(function (definition, index) {
-          return (
-            <div key={index}>
-              <p className="definition mt-1">{definition.definition}</p>
-              <p className="example">
-                <em>{definition.example}</em>
-              </p>
-              <Synonyms synonyms={definition.synonyms} />
-            </div>
-          );
-        })}
-      </div>
+      <section>
+        <h4 className="part-of-speech">{props.meaning.partOfSpeech}</h4>
+        <div>
+          {props.meaning.definitions.map(function (definition, index) {
+            return (
+              <div key={index}>
+                <p className="definition">{definition.definition}</p>
+                <p className="example">
+                  <em>{definition.example}</em>
+                </p>
+                <Synonyms synonyms={definition.synonyms} />
+              </div>
+            );
+          })}
+        </div>
+      </section>
     </div>
   );
 }
--- src/Results.js
+++ src/Results.js
@@ -6,8 +6,10 @@ export default function Results(props) {
   if (props.data) {
     return (
       <div className="Results">
-        <h2>{props.data.word}</h2>
-        <Phonetics phonetics={props.data.phonetics} />
+        <section>
+          <h2>{props.data.word}</h2>
+          <Phonetics phonetics={props.data.phonetics} />{" "}
+        </section>
         <Meanings meanings={props.data.meanings} />
       </div>
     );
